0cabb77fd89f54a98fe9fe590bf869abda881c41
Diana V <drv687@users.noreply.github.com>
2017-04-22 15:03:31 -0400

    Updating rss.js once again

--- rss.js
+++ rss.js
@@ -1,45 +1,59 @@
-const path = require('path')
-const{dialog} = require('electron').remote
-console.log(dialog)
+const notifier = require('node-notifier');
+const path = require('path');
+
 $("#help").hide();
 $("#showxml").click(function () {
 
-	var feedurl = $("#inputRSS").val();
+                var feedurl = $("#inputRSS").val();
+                var vTitle = "TITLE";
+                var vDescription = "DESC";
+                if (feedurl == "") {
+                                $("#help").fadeIn();
+                                throw new Error("No RSS feed url");
+                } else {
+                                $("#results").text("");
+                                $("#help").fadeOut();
+                                $("<li>"+feedurl+"</li>").appendTo("#favs");
+                                $.get(feedurl, function (data) {
+                                                var $XML = $(data);
+                                                //show all data in console
+                                                console.log(data);
+                                                $XML.find("item").each(function () {
+                                                                var $this = $(this),
+                                                                                item = {
+                                                                                                title: $this.find("title").text(),
+                                                                                                link: $this.find("link").text(),
+                                                                                                description: $this.find("description").text(),
+                                                                                                pubDate: $this.find("pubDate").text(),
+                                                                                                author: $this.find("author").text()
+                                                                                };
+                                                                vTitle = item.title;
+                                                                vDescription = item.description;
+                                                                vLink = item.link; //Adds a link to the notification
+$('#results').append($('<div class="panel panel-default"/>').html('<div class="panel-body"><p><strong><a class="itemClick"><span class="title">' + item.title + '</span></a></strong></p>' + '<p>' + $('<div class ="panel panel-default" />').html('<div class="panel-body>"<a class="itemClick"><span class ="message">'+ item.description+'</p></div>'))
 
-	if (feedurl == "") {
-		$("#help").fadeIn();
-		throw new Error("No RSS feed url");
-	} else {
-		$("#results").text("");
-		$("#help").fadeOut();
-		$("<li>"+feedurl+"</li>").appendTo("#favs");
-		$.get(feedurl, function (data) {
-			var $XML = $(data);
-			//show all data in console
-			console.log(data);
-			$XML.find("item").each(function () {
-				var $this = $(this),
-					item = {
-						title: $this.find("title").text(),
-						link: $this.find("link").text(),
-						description: $this.find("description").text(),
-						pubDate: $this.find("pubDate").text(),
-						author: $this.find("author").text()
-					};
-				$('#results').append($('<div class="panel panel-default"/>').html('<div class="panel-body"><p><strong><a class="itemClick"><span class="title">' + item.title + '</span></a></strong></p>' + '<p>' + $('<div class ="panel panel-default" />').html('<div class="panel-body>"<a class="itemClick"><span class ="message">'+ item.description +trimdata(item.description) + '</p></div>')));
-			});
-		});
-	}
+                                                );
+                                                });
+                                                //
+                                                notifier.notify({
+                                                  title: vTitle,
+                                                  message: vDescription,
+                                                  url: vLink,
+                                                  sound: true, // Only Notification Center or Windows Toasters
+                                                  wait: true // Wait with callback, until user action is taken against notification
+                                                }, function (err, response) {
+                                                  // Response is response from notification
+                                                });
+
+                                                notifier.on('click', function (notifierObject, options) {
+                                                  // Triggers if `wait: true` and user clicks notification
+                                                });
+
+                                                notifier.on('timeout', function (notifierObject, options) {
+                                                  // Triggers if `wait: true` and notification closes
+                                                });
+                                });
+
+                }
+
 });
-function trimdata(txt){
-	var ttxt = txt.substring(0,140);
-	return ttxt + "...";
-}
-
-	$("#results").on( "click",".itemClick", function(){
-		var title = "RSS FEED!",
-            message = $(this).find(".title").text();
-						description = $(this).find(".description").text();
-						link = $(this).find(".link").text();
-dialog.showMessageBox({ message: "Your rss feed is: " this, buttons: ["OK"]});
-	});
